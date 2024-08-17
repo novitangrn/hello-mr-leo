596968bab92e96ceff7c41e8231a08b631173396
Novita Anggraini <90380785+novitangrn@users.noreply.github.com>
2024-08-17 11:53:37 +0700

    back to latest layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,22 +33,22 @@ if st.session_state.page == 1:
                 rate_21 = st.slider('rate how was ur 21', min_value=0, max_value=10)
                 
                 # Pertanyaan 2
-                fav_memory = st.text_input('fav memory di 21', label_visibility='collapsed', key='fav_memory')
+                fav_memory = st.text_input('fav memory di 21', key='fav_memory')
         
                 # Pertanyaan 3
-                worst_memory = st.text_input('worst memory di 21', label_visibility='collapsed', key='worst_memory')
+                worst_memory = st.text_input('worst memory di 21', key='worst_memory')
         
                 # Pertanyaan 4
-                best_place = st.text_input('tempat terbaik yang dikunjungi di 21', label_visibility='collapsed', key='best_place')
+                best_place = st.text_input('tempat terbaik yang dikunjungi di 21', key='best_place')
         
                 # Pertanyaan 5
-                best_food = st.text_input('makanan terenak yang dicoba di 21', label_visibility='collapsed', key='best_food')
+                best_food = st.text_input('makanan terenak yang dicoba di 21', key='best_food')
         
                 # Pertanyaan 6
-                best_movie = st.text_input('film terbagus yang ditonton di 21', label_visibility='collapsed', key='best_movie')
+                best_movie = st.text_input('film terbagus yang ditonton di 21', key='best_movie')
         
                 # Pertanyaan 7
-                coolest_project = st.text_input('project terkeren yang dibuat di 21', label_visibility='collapsed', key='coolest_project')
+                coolest_project = st.text_input('project terkeren yang dibuat di 21', key='coolest_project')
         
                 submit_button = st.form_submit_button(label='Submit')
     
