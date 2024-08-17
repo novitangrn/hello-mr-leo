8729b6fad9f9ec48c48a5e3e665e5d859761a8bd
Novita Anggraini <90380785+novitangrn@users.noreply.github.com>
2024-08-17 11:30:51 +0700

    add form

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,52 +24,47 @@ if st.session_state.page == 1:
 
     # Tombol untuk memunculkan pop-up
     if st.button("lezgo"):
-        st.write("Play duluuu biar ga sepi")
+        st.write("play duluuu biar ga sepi")
         st.audio(audio_bytes, format='audio/mp3')
         
         with st.expander("survey about ur 21"):
-            
-            # Pertanyaan 1
-            st.write("rate how was ur 21")
-            st.slider('1-10', min_value=0, max_value=10)
-            
-            # Pertanyaan 2
-            st.write("fav memory di 21")
-            st.text_input('', key='fav_memory')
-    
-            # Pertanyaan 3
-            st.write("worst memory di 21")
-            st.text_input('', key='worst_memory')
-    
-            # Pertanyaan 4
-            st.write("tempat terbaik yang dikunjungi di 21")
-            st.text_input('', key='best_place')
-    
-            # Pertanyaan 5
-            st.write("makanan terenak yang dicoba di 21")
-            st.text_input('', key='best_food')
-    
-            # Pertanyaan 6
-            st.write("film terbagus yang ditonton di 21")
-            st.text_input('', key='best_movie')
-    
-            # Pertanyaan 7
-            st.write("project terkeren yang dibuat di 21")
-            st.text_input('', key='coolest_project')
-    
-            # Pertanyaan terakhir
-            st.write("janji jangan pernah nyerah di 22")
-            col1, col2 = st.columns(2)
-            with col1:
-                if st.button("ya"):
-                    st.success("yey.")
-            with col2:
-                if st.button("engga"):
-                    st.warning("aslinya ini iya cuma tulisannya engga.") 
+            with st.form(key='survey_form'):
+                # Pertanyaan 1
+                st.write("rate how was ur 21")
+                rate_21 = st.slider('1-10', min_value=0, max_value=10)
+                
+                # Pertanyaan 2
+                st.write("fav memory di 21")
+                fav_memory = st.text_input('', key='fav_memory')
+        
+                # Pertanyaan 3
+                st.write("worst memory di 21")
+                worst_memory = st.text_input('', key='worst_memory')
+        
+                # Pertanyaan 4
+                st.write("tempat terbaik yang dikunjungi di 21")
+                best_place = st.text_input('', key='best_place')
+        
+                # Pertanyaan 5
+                st.write("makanan terenak yang dicoba di 21")
+                best_food = st.text_input('', key='best_food')
+        
+                # Pertanyaan 6
+                st.write("film terbagus yang ditonton di 21")
+                best_movie = st.text_input('', key='best_movie')
+        
+                # Pertanyaan 7
+                st.write("project terkeren yang dibuat di 21")
+                coolest_project = st.text_input('', key='coolest_project')
+        
+                submit_button = st.form_submit_button(label='Submit')
     
             # Opsional: Tombol untuk melanjutkan ke halaman berikutnya (jika menggunakan navigasi halaman)
-            if st.button("Next"):
-                next_page()
+            if submit_button:
+                st.success("u did well di 21!")
+                if st.button("Next"):
+                    next_page()
+                    
 # Halaman 2
 elif st.session_state.page == 2:
     st.title("Halaman 2")
